f2d7d3d2ad3b27b09caae0ba09d47e4dad901a90
Rohit Goswami <rog32@hi.is>
2023-10-27 23:06:28 +0000

    ENH: Rework and add more minima handling

--- scripts/plot_2d.py
+++ scripts/plot_2d.py
@@ -2,10 +2,21 @@ import numpy as np
 from cmcrameri import cm
 import matplotlib.pyplot as plt
 import argparse
+from adjustText import adjust_text
 
 # Set up command-line argument parsing
-parser = argparse.ArgumentParser(description="Load and plot grid data from an NPZ file.")
-parser.add_argument("filename", type=str, help="The path to the NPZ file containing grid data.")
+parser = argparse.ArgumentParser(
+    description="Load and plot grid data from an NPZ file."
+)
+parser.add_argument(
+    "filename", type=str, help="The path to the NPZ file containing grid data."
+)
+parser.add_argument(
+    "--num_minima",
+    type=int,
+    default=1,
+    help="The number of minima to identify and plot.",
+)
 args = parser.parse_args()
 
 # Load data from the specified file
@@ -15,18 +26,42 @@ Y = grid.get("y")
 Z = grid.get("z")
 
 # Identify the minima
-min_val = np.min(Z)
-minima_coords = np.where(Z == min_val)
+sorted_indices = np.argsort(Z, axis=None)[: args.num_minima]
+top_minima_coords = [np.unravel_index(index, Z.shape) for index in sorted_indices]
 
 # Plotting
 plt.figure(figsize=(12, 9))
 plt.contourf(X, Y, Z, 50, cmap=cm.batlow, alpha=0.8)
 plt.colorbar()
 
-# Minima
-plt.scatter(
-    X[minima_coords], Y[minima_coords], marker="*", color="black", s=200, label="Minima"
-)
+texts = []
+# Plot the identified minima and add text labels
+for i, coords in enumerate(top_minima_coords):
+    x_coord = X[coords]
+    y_coord = Y[coords]
+    z_val = Z[coords]
+    plt.scatter(
+        x_coord,
+        y_coord,
+        marker="*",
+        color="black",
+        s=200,
+        label="Minima" if i == 0 else "",
+    )
+    texts.append(
+        plt.text(
+            x_coord,
+            y_coord,
+            f"M{i} ({x_coord:.2f}, {y_coord:.2f}, {z_val:.2f})",
+            fontsize=9,
+            color="white",
+        )
+    )
+
+# Repel the labels to avoid overlaps
+adjust_text(texts, arrowprops=dict(arrowstyle="-", color="k"))
+
+# If multiple minima, only label the first one to prevent legend duplicates
 plt.legend()
 
 plt.show()
