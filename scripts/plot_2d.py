3fd2a19f644ebbd982dd451916d05b5b29c8ecaa
Rohit Goswami <rog32@hi.is>
2023-10-27 23:22:19 +0000

    ENH: Add an exclusion_radius to the plotter

--- scripts/plot_2d.py
+++ scripts/plot_2d.py
@@ -5,18 +5,10 @@ import argparse
 from adjustText import adjust_text
 
 # Set up command-line argument parsing
-parser = argparse.ArgumentParser(
-    description="Load and plot grid data from an NPZ file."
-)
-parser.add_argument(
-    "filename", type=str, help="The path to the NPZ file containing grid data."
-)
-parser.add_argument(
-    "--num_minima",
-    type=int,
-    default=1,
-    help="The number of minima to identify and plot.",
-)
+parser = argparse.ArgumentParser(description="Load and plot grid data from an NPZ file.")
+parser.add_argument("filename", type=str, help="The path to the NPZ file containing grid data.")
+parser.add_argument("--num_minima", type=int, default=1, help="The number of minima to identify and plot.")
+parser.add_argument("--exclusion_radius", type=float, default=0.1, help="The exclusion radius around each minimum on function values.")
 args = parser.parse_args()
 
 # Load data from the specified file
@@ -25,9 +17,18 @@ X = grid.get("x")
 Y = grid.get("y")
 Z = grid.get("z")
 
-# Identify the minima
-sorted_indices = np.argsort(Z, axis=None)[: args.num_minima]
-top_minima_coords = [np.unravel_index(index, Z.shape) for index in sorted_indices]
+# Identify the minima with exclusion zones
+minima_coords = []
+z_copy = np.copy(Z)
+
+for _ in range(args.num_minima):
+    min_val_index = np.argmin(z_copy)
+    min_coord = np.unravel_index(min_val_index, z_copy.shape)
+    minima_coords.append(min_coord)
+
+    # Mask the area around the minima
+    exclusion_zone = np.where(np.abs(Z - Z[min_coord]) < args.exclusion_radius)
+    z_copy[exclusion_zone] = np.inf
 
 # Plotting
 plt.figure(figsize=(12, 9))
@@ -36,27 +37,12 @@ plt.colorbar()
 
 texts = []
 # Plot the identified minima and add text labels
-for i, coords in enumerate(top_minima_coords):
+for i, coords in enumerate(minima_coords):
     x_coord = X[coords]
     y_coord = Y[coords]
     z_val = Z[coords]
-    plt.scatter(
-        x_coord,
-        y_coord,
-        marker="*",
-        color="black",
-        s=200,
-        label="Minima" if i == 0 else "",
-    )
-    texts.append(
-        plt.text(
-            x_coord,
-            y_coord,
-            f"M{i} ({x_coord:.2f}, {y_coord:.2f}, {z_val:.2f})",
-            fontsize=9,
-            color="white",
-        )
-    )
+    plt.scatter(x_coord, y_coord, marker="*", color="black", s=200, label="Minima" if i == 0 else "")
+    texts.append(plt.text(x_coord, y_coord, f"M{i} ({x_coord:.2f}, {y_coord:.2f}, {z_val:.2f})", fontsize=9, color="white"))
 
 # Repel the labels to avoid overlaps
 adjust_text(texts, arrowprops=dict(arrowstyle="-", color="k"))
