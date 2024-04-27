6380954b43dbfeebe6d43803cb2578321f018f9a
Rohit Goswami <rog32@hi.is>
2024-04-27 00:47:25 +0000

    MAINT: Lint and update XTPot

--- CppCore/xtsci/pot/base.hpp
+++ CppCore/xtsci/pot/base.hpp
@@ -12,6 +12,7 @@
 
 #include "rgpot/Potential.hpp"
 #include "rgpot/types/adapters/xtensor.hpp"
+#include "xtensor/xtensor_forward.hpp"
 #include "xtsci/func/base.hpp"
 #include <xtensor/xadapt.hpp>
 #include <xtensor/xindex_view.hpp>
@@ -23,13 +24,14 @@ template <typename ScalarType = double>
 class XTPot : public func::ObjectiveFunction<ScalarType> {
   friend class TestXTPot; // Make a test class a friend
 public:
-  XTPot(std::shared_ptr<rgpot::Potential> pot,
-        const xt::xtensor<ScalarType, 2> &base_pos,
-        const xt::xtensor<int, 1> &atomTypes,
-        const xt::xtensor<double, 2> &boxMatrix,
-        const xt::xtensor<bool, 1> &fixedMask = {})
+  XTPot(
+      std::shared_ptr<rgpot::Potential> pot,
+      const xt::xtensor<ScalarType, 2> &base_pos,
+      const xt::xtensor<int, 1> &atomTypes,
+      const xt::xtensor<double, 2> &boxMatrix,
+      const xt::xtensor<bool, 1> &fixedMask = {})
       : func::ObjectiveFunction<ScalarType>(
-            atomTypes.size() * 3, expandFixedMask(fixedMask, atomTypes.size())),
+          atomTypes.size() * 3, expandFixedMask(fixedMask, atomTypes.size())),
         m_pot(pot), m_basepos(base_pos),
         m_atomTypes(rgpot::types::adapt::xtensor::convertToVector(atomTypes)),
         m_box(rgpot::types::adapt::xtensor::convertToArray3x3(boxMatrix)) {}
@@ -44,7 +46,7 @@ protected: // Useful to test the damn thing
   xt::xtensor<ScalarType, 2>
   reconstruct_full(const xt::xarray<ScalarType> &free_x) const {
     std::array<std::size_t, 2> shape = {m_atomTypes.size(), 3};
-    xt::xtensor<double, 2> allpos = xt::reshape_view(m_basepos, shape);
+    xt::xtensor<double, 2> allpos    = xt::reshape_view(m_basepos, shape);
     // Flatten all structures for simpler indexing
     auto flat_indices = xt::from_indices(xt::nonzero(m_free));
     // Check the size of free_x to match the flat indices count
@@ -63,42 +65,36 @@ private:
   const xt::xtensor<bool, 1> m_free{!this->m_isFixed};
   xt::xtensor<double, 2> m_basepos;
 
-  ScalarType compute(const xt::xarray<ScalarType> &x) const override {
-    xt::xtensor<double, 2> positions = this->reshape_x_to_positions(x);
+  ScalarType compute(const xt::xarray<ScalarType> &free_x) const override {
+    xt::xtensor<double, 2> positions = this->reconstruct_full(free_x);
 
-    auto [energy, forces] =
-        (*m_pot)(rgpot::types::adapt::xtensor::convertToAtomMatrix(positions),
-                 m_atomTypes, m_box);
+    auto [energy, forces] = (*m_pot)(
+        rgpot::types::adapt::xtensor::convertToAtomMatrix(positions),
+        m_atomTypes, m_box);
 
     return energy;
   }
 
   std::optional<xt::xarray<ScalarType>>
-  compute_gradient(const xt::xarray<ScalarType> &x) const override {
-    xt::xtensor<double, 2> positions = this->reshape_x_to_positions(x);
+  compute_gradient(const xt::xarray<ScalarType> &free_x) const override {
+    xt::xtensor<double, 2> positions = this->reconstruct_full(free_x);
 
-    auto [energy, forces] =
-        (*m_pot)(rgpot::types::adapt::xtensor::convertToAtomMatrix(positions),
-                 m_atomTypes, m_box);
+    auto [energy, forces] = (*m_pot)(
+        rgpot::types::adapt::xtensor::convertToAtomMatrix(positions),
+        m_atomTypes, m_box);
 
     std::array<size_t, 2> shape = {m_atomTypes.size(), 3};
-    auto forces_xtensor =
-        xt::adapt(forces.data(), shape, xt::layout_type::row_major);
+    auto forces_xtensor
+        = xt::adapt(forces.data(), shape, xt::layout_type::row_major);
 
     // Convert forces to gradient
-    auto gradient = -1.0 * forces_xtensor;
-    auto flattened_gradient = xt::flatten(gradient);
-    return flattened_gradient;
+    auto gradient       = -1.0 * forces_xtensor;
+    auto flat_free_grad = get_free(xt::flatten(gradient));
+    return flat_free_grad;
   }
 
-  xt::xtensor<double, 2>
-  reshape_x_to_positions(const xt::xarray<ScalarType> &x) const {
-    std::array<std::size_t, 2> shape = {m_atomTypes.size(), 3};
-    return xt::reshape_view(x, shape);
-  }
-
-  xt::xtensor<bool, 1> expandFixedMask(const xt::xtensor<bool, 1> &mask,
-                                       size_t numMolecules) {
+  xt::xtensor<bool, 1>
+  expandFixedMask(const xt::xtensor<bool, 1> &mask, size_t numMolecules) {
     if (mask.size() != numMolecules && mask.size() != 0) {
       throw std::runtime_error("Incorrect size of the fixed mask tensor.");
     }
